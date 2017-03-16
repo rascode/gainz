1aef6afe3110246a8aa720fe4384168908ac064c
H. Justin Rascoe <inbox@justinrascoe.com>
2017-03-15 22:30:10 -0400

    Initial commit with functional, out-of-the-box iOS single page application

---
+++ Gainz/Gainz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gainz
+//
+//  Created by Rascoe, Justin on 3/15/17.
+//  Copyright © 2017 Rascode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
