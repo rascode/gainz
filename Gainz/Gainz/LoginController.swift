dc07c3d84f687bf1516646a4ed3d83d65ab7ed17
H. Justin Rascoe <inbox@justinrascoe.com>
2017-03-15 23:19:01 -0400

    Added Login Controller File

---
+++ Gainz/Gainz/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  Gainz
+//
+//  Created by Rascoe, Justin on 3/15/17.
+//  Copyright © 2017 Rascode. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 135/255, green: 163/255, blue: 69/255, alpha: 1)
+    }
+
+}
